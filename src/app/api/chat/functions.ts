10208371b715eb22da0fe4358d28c7a2d054c4ab
adian-avang <adian.cm@avangenio.com>
2024-04-12 01:29:49 -0400

    test create event

--- src/app/api/chat/functions.ts
+++ src/app/api/chat/functions.ts
@@ -1,117 +1,139 @@
 export const functions: any[] = [
   {
-    name: "get_top_stories",
-    description:
-      "Get the top stories from Hacker News. Also returns the Hacker News URL to each story.",
+    name: 'create_event_in_calendar',
+    description: 'Create an event in Noswork Calendar',
     parameters: {
-      type: "object",
-      properties: {
-        limit: {
-          type: "number",
-          description: "The number of stories to return. Defaults to 10.",
-        },
-      },
-      required: [],
-    },
-  },
-  {
-    name: "get_story",
-    description:
-      "Get a story from Hacker News. Also returns the Hacker News URL to the story.",
-    parameters: {
-      type: "object",
-      properties: {
-        id: {
-          type: "number",
-          description: "The ID of the story",
-        },
-      },
-      required: ["id"],
-    },
-  },
-  {
-    name: "get_story_with_comments",
-    description:
-      "Get a story from Hacker News with comments.  Also returns the Hacker News URL to the story and each comment.",
-    parameters: {
-      type: "object",
-      properties: {
-        id: {
-          type: "number",
-          description: "The ID of the story",
-        },
-      },
-      required: ["id"],
-    },
-  },
-  {
-    name: "summarize_top_story",
-    description:
-      "Summarize the top story from Hacker News, including both the story and its comments. Also returns the Hacker News URL to the story and each comment.",
-    parameters: {
-      type: "object",
+      type: 'object',
       properties: {},
       required: [],
     },
   },
-];
+  // {
+  //   name: 'get_story',
+  //   description:
+  //     'Get a story from Hacker News. Also returns the Hacker News URL to the story.',
+  //   parameters: {
+  //     type: 'object',
+  //     properties: {
+  //       id: {
+  //         type: 'number',
+  //         description: 'The ID of the story',
+  //       },
+  //     },
+  //     required: ['id'],
+  //   },
+  // },
+  // {
+  //   name: 'get_story_with_comments',
+  //   description:
+  //     'Get a story from Hacker News with comments.  Also returns the Hacker News URL to the story and each comment.',
+  //   parameters: {
+  //     type: 'object',
+  //     properties: {
+  //       id: {
+  //         type: 'number',
+  //         description: 'The ID of the story',
+  //       },
+  //     },
+  //     required: ['id'],
+  //   },
+  // },
+  // {
+  //   name: 'summarize_top_story',
+  //   description:
+  //     'Summarize the top story from Hacker News, including both the story and its comments. Also returns the Hacker News URL to the story and each comment.',
+  //   parameters: {
+  //     type: 'object',
+  //     properties: {},
+  //     required: [],
+  //   },
+  // },
+]
 
-async function get_top_stories(limit: number = 10) {
-  const response = await fetch(
-    "https://hacker-news.firebaseio.com/v0/topstories.json",
-  );
-  const ids = await response.json();
-  const stories = await Promise.all(
-    ids.slice(0, limit).map((id: number) => get_story(id)),
-  );
-  return stories;
-}
+// async function get_top_stories(limit: number = 10) {
+//   console.log('getting top stories')
+//   const response = await fetch(
+//     // "https://hacker-news.firebaseio.com/v0/topstories.json",
+//     'https://rickandmortyapi.com/api/episode/28'
+//   )
+//   const ids = await response.json()
+//   // const stories = await Promise.all(
+//   //   ids.slice(0, limit).map((id: number) => get_story(id)),
+//   // );
+//   return {
+//     ...ids,
+//     hnUrl: `https://rickandmortyapi.com/api/episode/28`,
+//   }
+// }
 
-async function get_story(id: number) {
-  const response = await fetch(
-    `https://hacker-news.firebaseio.com/v0/item/${id}.json`,
-  );
-  const data = await response.json();
+async function create_event_in_calendar() {
+  const calendarUrl =
+    'https://workspaces.avangenio.com/apps/calendar/dayGridMonth/now'
+  const url = 'https://rickandmortyapi.com/api/episode/28'
+  const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    // body: JSON.stringify({}),
+  }
+
+  const response = await fetch(url, options)
+
+  const ids = await response.json()
   return {
-    ...data,
-    hnUrl: `https://news.ycombinator.com/item?id=${id}`,
-  };
+    ...ids,
+    hnUrl: calendarUrl,
+  }
 }
+// async function get_story(id: number) {
+//   console.log('getting story', id)
+//   const response = await fetch(
+//     `https://hacker-news.firebaseio.com/v0/item/${id}.json`
+//   )
+//   const data = await response.json()
+//   return {
+//     ...data,
+//     hnUrl: `https://news.ycombinator.com/item?id=${id}`,
+//   }
+// }
 
-async function get_story_with_comments(id: number) {
-  const response = await fetch(
-    `https://hacker-news.firebaseio.com/v0/item/${id}.json`,
-  );
-  const data = await response.json();
-  const comments = await Promise.all(
-    data.kids.slice(0, 10).map((id: number) => get_story(id)),
-  );
-  return {
-    ...data,
-    hnUrl: `https://news.ycombinator.com/item?id=${id}`,
-    comments: comments.map((comment: any) => ({
-      ...comment,
-      hnUrl: `https://news.ycombinator.com/item?id=${comment.id}`,
-    })),
-  };
-}
+// async function get_story_with_comments(id: number) {
+//   console.log('getting story with comments', id)
+//   const response = await fetch(
+//     `https://hacker-news.firebaseio.com/v0/item/${id}.json`
+//   )
+//   const data = await response.json()
+//   const comments = await Promise.all(
+//     data.kids.slice(0, 10).map((id: number) => get_story(id))
+//   )
+//   return {
+//     ...data,
+//     hnUrl: `https://news.ycombinator.com/item?id=${id}`,
+//     comments: comments.map((comment: any) => ({
+//       ...comment,
+//       hnUrl: `https://news.ycombinator.com/item?id=${comment.id}`,
+//     })),
+//   }
+// }
 
-async function summarize_top_story() {
-  const topStory = await get_top_stories(1);
-  return await get_story_with_comments(topStory[0].id);
-}
+// async function summarize_top_story() {
+//   console.log('summarizing top story')
+//   const topStory = await get_top_stories(1)
+//   return await get_story_with_comments(topStory[0].id)
+// }
 
 export async function runFunction(name: string, args: any) {
   switch (name) {
-    case "get_top_stories":
-      return await get_top_stories();
-    case "get_story":
-      return await get_story(args["id"]);
-    case "get_story_with_comments":
-      return await get_story_with_comments(args["id"]);
-    case "summarize_top_story":
-      return await summarize_top_story();
+    case 'create_event_in_calendar':
+      return await create_event_in_calendar()
+    // case 'get_story':
+    //   return await get_story(args['id'])
+    // case 'get_story_with_comments':
+    //   return await get_story_with_comments(args['id'])
+    // case 'summarize_top_story':
+    //   return await summarize_top_story()
     default:
-      return null;
+      return null
   }
 }
